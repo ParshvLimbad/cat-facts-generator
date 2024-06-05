c17a140738d948d70122634d16008f2d3ab37506
Parshv Limbad <103508696+ParshvLimbad@users.noreply.github.com>
2024-06-05 18:20:10 +0530

    Integrated cat facts api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Card, CardTitle } from "@/components/ui/card";
-import Image from "next/image";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Home() {
+  const [fact, setFact] = useState("");
+  const getFact = () => {
+    axios
+      .get("https://meowfacts.herokuapp.com/")
+      .then((res) => {
+        console.log(res.data.data[0]);
+        setFact(res.data.data[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <main className="w-full h-screen flex items-center justify-center">
-      <Card className="w-[400px] h-[250px] p-5 flex flex-col gap-5 justify-center">
+      <Card className="w-[400px] p-5 flex flex-col gap-5 justify-center">
         <CardTitle>Generate a cat fact</CardTitle>
         <div className="h-full rounded-lg border bg-card text-card-foreground p-3 flex justify-center">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat
-          quisquam nemo vitae magnam accusamus
+          <p>{fact}</p>
         </div>
         <div className="flex w-full justify-end">
           <Button
@@ -18,6 +32,7 @@ export default function Home() {
               backgroundColor: "#f5dc00",
               color: "black",
             }}
+            onClick={getFact}
           >
             Generate
           </Button>
